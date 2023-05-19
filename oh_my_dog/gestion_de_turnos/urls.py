bd521ec21f6c08d08255eee34fa10c9952f83bef
FacuLede <facundo_lede@hotmail.com>
2023-05-19 11:22:26 -0300

    Implementé HU ver turnos de clientes

--- oh_my_dog/gestion_de_turnos/urls.py
+++ oh_my_dog/gestion_de_turnos/urls.py
@@ -3,4 +3,5 @@ from gestion_de_turnos import views
 
 urlpatterns = [
     path('sacar_turno',views.sacar_turno, name= "sacar_turno"),
+    path('turnos_pendientes',views.turnos_pendientes, name= "turnos_pendientes"),
 ]
--- oh_my_dog/gestion_de_turnos/views.py
+++ oh_my_dog/gestion_de_turnos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import Turno_form
+from .models import Turno
 
 # Create your views here.
 
@@ -15,3 +16,7 @@ def sacar_turno(request):
             data["mensaje"] = "Se solicitó el turno correctamente."              
     
     return render(request,"gestion_de_turnos/sacar_turno.html",data)
+
+def turnos_pendientes(request):
+    turnos_pendientes=Turno.objects.filter(estado = "Pendiente")
+    return render(request,"gestion_de_turnos/turnos_pendientes.html",{"turnos":turnos_pendientes})
